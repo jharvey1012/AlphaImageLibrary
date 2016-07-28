31d6403dddff534f31167c2ecac2b6b1684f2d76
Harvey <harveyjo@oregonstate.edu>
2016-07-28 13:28:05 -0700

    Added ability to watermark an image with an overlay of another image. Refractored code for modularity.

--- Image.php
+++ Image.php
@@ -1,70 +1,73 @@
 <?php
 
 class Image {
+
     private $img;
     private $manipImg;
     private $resizeWidth;
     private $resizeHeight;
+    private $watermark;
     private $newImage;
     private $type;
     private $width;
     private $height;
     private $size;
     private $path;
-    
-    function __construct($path) {   
-        
-       $this->path = $path;    
-       $this->determineType();
-       
-       list($width, $height) = getimagesize($this->path);
-       
-       $this->setWidth($width);
-       $this->setHeight($height);
-       
-       $this->size = filesize($this->path);
+
+    function __construct($path) {
+        $this->path = $path;
+        $this->type = $this->determineType();
+        $this->img = $this->loadImage($this->type);
+        list($width, $height) = getimagesize($this->path);
+        $this->setWidth($width);
+        $this->setHeight($height);
+        $this->size = filesize($this->path);
     }
-    
+
     public function setHeight($height) {
         $this->height = $height;
     }
-    
+
     public function getHeight() {
         return $this->height;
     }
-    
+
     public function setWidth($width) {
         $this->width = $width;
     }
     
+    public function getImgResource() {
+        return $this->img;
+    }
+
     public function getWidth() {
         return $this->width;
     }
-    
+
     private function determineType() {
         $typeCode = exif_imagetype($this->path);
-        
-        switch($typeCode) {
+
+        switch ($typeCode) {
             case 1:
-                $this->type = "image/gif";
-                $this->img = imagecreatefromgif($this->path);
+                return "image/gif";
+                // $this->img = imagecreatefromgif($this->path);
                 break;
             case 2:
                 // JPEG
-                $this->type = "image/jpeg";
-                $this->img = imagecreatefromjpeg($this->path);
+                return "image/jpeg";
+                //  $this->img = imagecreatefromjpeg($this->path);
 //                $this->didOpenSuccessfully($this->img);
                 break;
-            case 3: 
+            case 3:
                 // PNG 
-                $this->type = "image/png";
-                $this->img = imagecreatefrompng($this->path);
+                return "image/png";
+                //  $this->img = imagecreatefrompng($this->path);
                 break;
             case 4:
-                // SWF @todo add support for this filetype
+                throw new Exception("SWF images aren't supported at this time.");
                 break;
             case 5:
-                // PSD @todo add support for this filetype / restrict resizing
+                throw new Exception("PSD images aren't supported at this time.");
                 break;
             case 6:
                 // BMP @todo add support for this filetype
@@ -78,22 +81,22 @@ class Image {
             case 9:
                 // JPC @todo add support for this filetype
                 break;
-            case 10: 
+            case 10:
                 // JP2 @todo add support for this filetype
                 break;
             case 11:
                 // JPX @todo add support for this filetype
                 break;
-            case 12: 
+            case 12:
                 // JB2 @todo add support for this filetype
                 break;
-            case 13: 
+            case 13:
                 // SWC @todo add support for this filetype
                 break;
             case 14:
                 // IFF @todo add support for this filetype
                 break;
-            case 15: 
+            case 15:
                 // WBMP @todo add support for this filetype
                 break;
             case 16:
@@ -102,28 +105,53 @@ class Image {
             case 17:
                 // ICO @todo add support for this filetype
                 break;
-            default: 
+            default:
                 throw new Exception("File is not an image");
         }
     }
-    
-    
+
+    private function loadImage($type, $path = null) {
+        switch ($this->type) {
+            case 'image/jpeg':
+                if (isset($path)) {
+                    return imagecreatefromjpeg($path);
+                } else {
+                     return imagecreatefromjpeg($this->path);
+                }
+                break;
+            case "image/png":
+                if (isset($path)) {
+                    return imagecreatefrompng($path);
+                } else {
+                    return imagecreatefrompng($this->path);
+                }
+                break;
+            case "image/gif":
+                if (isset($path)) {
+                    return imagecreatefromgif($path);
+                } else {
+                    return imagecreatefromgif($this->path);
+                }
+                break;
+        }
+    }
+
     public function createNewImage($width, $height) {
         $this->newImage = imagecreatetruecolor($width, $height);
         imagecopyresampled($this->newImage, $this->img, 0, 0, 0, 0, $this->resizeWidth, $this->resizeHeight, $this->width, $this->height);
+        $this->img = $this->newImage;
     }
-    
-    
+
     public function resizeExact($width, $height) {
         $this->createNewImage($width, $height);
     }
-    
+
     public function resizeMaxHeight($height) {
         $this->resizeWidth = $this->resizeWidthByHeight($height);
         $this->resizeHeight = $height;
         $this->createNewImage($this->resizeWidth, $this->resizeHeight);
     }
-    
+
     public function resizeMaxWidth($width) {
         $this->resizeHeight = $this->resizeHeightByWidth($width);
         $this->resizeWidth = $width;
@@ -131,67 +159,79 @@ class Image {
     }
 
     private function didOpenSuccessfully($resource) {
-        if(!$resource) {
-           throw Exception("Couldn't load the specified image.");
+        if (!$resource) {
+            throw Exception("Couldn't load the specified image.");
         }
     }
-    
-    
+
     // Credit: https://paulund.co.uk/resize-image-class-php
     private function resizeHeightByWidth($width) {
         return floor(($this->height / $this->width) * $width);
     }
-    
+
     private function resizeWidthByHeight($height) {
-       return floor(($this->width / $this->height) * $height); 
+        return floor(($this->width / $this->height) * $height);
     }
-    
+
     private function setPNGCompression($quality) {
         return ($quality / 100) * 9;
     }
-    
+
     public function saveImage($path, $quality, $download = false) {
-        switch($this->type) {
+        switch ($this->type) {
             case 'image/jpeg':
-                if(imagetypes() & IMG_JPG) {
-                    imagejpeg($this->newImage, $path, $quality);
-                }
-                else {
+                if (imagetypes() & IMG_JPG) {
+                    imagejpeg($this->img, $path, $quality);
+                } else {
                     throw Exception("JPEG image handling not enabled in your PHP settings.");
                 }
                 break;
             case "image/png":
                 $compression = $this->setPNGCompression($quality);
-                if(imagetypes() & IMG_PNG) {
-                    imagepng($this->newImage, $path, $compression);
-                }
-                else {
+                if (imagetypes() & IMG_PNG) {
+                    imagepng($this->img, $path, $compression);
+                } else {
                     throw Exception("PNG image handling not enabled in your PHP settings.");
                 }
                 break;
             case "image/gif":
-                if(imagetypes() & IMG_GIF) {
-                    imagegif($this->newImage, $path); 
-                }
-                else {
+                if (imagetypes() & IMG_GIF) {
+                    imagegif($this->img, $path);
+                } else {
                     throw Exception("GIF image handling not enabled in your PHP settings.");
                 }
                 break;
         }
-            if($download)
-	    {
-	    	header('Content-Description: File Transfer');
-		header("Content-type: application/octet-stream");
-		header("Content-disposition: attachment; filename= ".$path."");
-		readfile($path);
-	    }
+        if ($download) {
+            header('Content-Description: File Transfer');
+            header("Content-type: application/octet-stream");
+            header("Content-disposition: attachment; filename= " . $path . "");
+            readfile($path);
+        }
+        imagedestroy($this->img);
     }
-    
+
+    public function watermarkFromPhoto($watermarkPath, $marginRight, $marginBottom) {
+        $this->watermark = new Image($watermarkPath);
+        $imgResource = $this->watermark->getImgResource();
+        $sx = imagesx($imgResource);
+        $sy = imagesy($imgResource);
+        $dsx = imagesx($this->img);
+        $dsy = imagesy($this->img);
+        $offsetRight = ($dsx - $sx - $marginRight);
+        $offsetBottom =  ($dsy - $sy - $marginBottom);
+        
+        imagecopy($this->img, $imgResource, $offsetRight, $offsetBottom, 0, 0, $sx, $sy);
+        
+    }
+
+    public function watermarkFromText() {
+        
+    }
+
 }
 
-
-
 // Ignore, for testing purposes. 
-$myImg = new Image('Rotating_Sphere.gif');
-$myImg->resizeMaxWidth(200);
-$myImg->saveImage("/new2.gif", 100, true);
+$myImg = new Image('home.jpg');
+$myImg->watermarkFromPhoto("Mushroom2.png", 10, 10);
+$myImg->saveImage("/new2.jpg", 100, true);
